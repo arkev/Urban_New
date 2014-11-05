233a7b21d17d8593fa27cab4052aa8440ee6d808
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2014-11-04 23:13:55 -0600

    varios cambios
    mejorar el menú, agregar los favicon, trabajar las funciones en un JS
    externo

---
+++ js/funciones.js
@@ -0,0 +1,67 @@
+$(document).on("ready", inicio);
+
+function inicio () 
+{
+	//Menú
+	$(".menu-opcion").click(function toggle(){
+		//quitar la clase .active al boton que la tenga:
+		$(".active").removeClass("active");
+		//agregar la clase .active al boton al que hicimos click:
+		$(this).addClass("active");
+	});	
+	// browser window scroll (in pixels) after which the "menu" link is shown
+	var offset = 300;
+
+	var navigationContainer = $('#cd-nav'),
+		mainNavigation = navigationContainer.find('#cd-main-nav ul');
+
+	//hide or show the "menu" link
+	checkMenu();
+	$(window).scroll(function(){
+		checkMenu();
+	});
+
+	//open or close the menu clicking on the bottom "menu" link
+	$('.cd-nav-trigger').on('click', function(){
+		$(this).toggleClass('menu-is-open');
+		//we need to remove the transitionEnd event handler (we add it when scolling up with the menu open)
+		mainNavigation.off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend').toggleClass('is-visible');
+
+	});
+
+	function checkMenu() {
+		if( $(window).scrollTop() > offset && !navigationContainer.hasClass('is-fixed')) {
+			navigationContainer.addClass('is-fixed').find('.cd-nav-trigger').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(){
+				mainNavigation.addClass('has-transitions');
+			});
+		} else if ($(window).scrollTop() <= offset) {
+			//check if the menu is open when scrolling up
+			if( mainNavigation.hasClass('is-visible')  && !$('html').hasClass('no-csstransitions') ) {
+				//close the menu with animation
+				mainNavigation.addClass('is-hidden').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+					//wait for the menu to be closed and do the rest
+					mainNavigation.removeClass('is-visible is-hidden has-transitions');
+					navigationContainer.removeClass('is-fixed');
+					$('.cd-nav-trigger').removeClass('menu-is-open');
+				});
+			//check if the menu is open when scrolling up - fallback if transitions are not supported
+			} else if( mainNavigation.hasClass('is-visible')  && $('html').hasClass('no-csstransitions') ) {
+					mainNavigation.removeClass('is-visible has-transitions');
+					navigationContainer.removeClass('is-fixed');
+					$('.cd-nav-trigger').removeClass('menu-is-open');
+			//scrolling up with menu closed
+			} else {
+				navigationContainer.removeClass('is-fixed');
+				mainNavigation.removeClass('has-transitions');
+			}
+		} 
+	}
+}
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		
+		  ga('create', 'UA-56321654-1', 'auto');
+		  ga('send', 'pageview');
